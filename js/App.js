b8909e88b5ae2e0c1f39bbc46c247fd778591e08
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-05-29 11:57:19 -0700

    exited scaffolding hell

---
+++ js/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <header>Testicles</header>
+        </div>
+    );
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: dist,
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    new CopyPlugin([
+      path.resolve(__dirname, "static")
+    ]),
+    new webpack.HotModuleReplacementPlugin(),
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};

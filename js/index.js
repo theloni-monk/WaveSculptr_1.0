f5a907c2aa35af8803e7db2a8204dae702ecc8c5
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-06-04 15:10:34 -0700

    ignore js build

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import("../pkg/index.js").catch(console.error);
-ReactDOM.render(React.createElement(React.StrictMode, null,
-    React.createElement(App, null)), document.getElementById('root'));
